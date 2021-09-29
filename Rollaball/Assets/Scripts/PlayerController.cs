c3012c22bc462a929e6eb93e47472f692c146f5b
Hane (Stella) Yie <hane.yie@gatech.edu>
2021-09-29 19:09:45 -0400

    m0 submitted

---
+++ Rollaball/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,70 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using TMPro;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 0;
+    public TextMeshProUGUI countText;
+    public GameObject winTextObject;
+    //change of script
+    public GameObject oneTextObject;
+
+    private Rigidbody rb;
+    private int count;
+    private float movementX;
+    private float movementY;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        count = 0;
+
+        SetCountText();
+        winTextObject.SetActive(false);
+        oneTextObject.SetActive(false);
+    }
+
+    void OnMove(InputValue movementValue)
+    {
+        Vector2 movementVector = movementValue.Get<Vector2>();
+
+        movementX = movementVector.x;
+        movementY = movementVector.y;
+    }
+
+    void SetCountText()
+    {
+        countText.text = "Count: " + count.ToString();
+        if(count >= 12)
+        {
+            winTextObject.SetActive(true);
+            oneTextObject.SetActive(false);
+        }
+        if (count == 11)
+        {
+            oneTextObject.SetActive(true);
+        }
+    }
+
+    void FixedUpdate()
+    {
+        Vector3 movement = new Vector3(movementX, 0.0f, movementY);
+
+        rb.AddForce(movement * speed);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("PickUp"))
+        {
+            other.gameObject.SetActive(false);
+            count = count + 1;
+
+            SetCountText(); 
+        }
+    }
+}
---
+++ Rollaball/Yie_H_m0_readme.txt
@@ -0,0 +1,27 @@
+﻿CS 4455 Milestone 0
+
+Name: Hane Yie
+Email: hane.yie@gatech.edu
+Account: hyie3
+
+Main Scene is MiniGame. Main build is OSX (Build/OSX) and Windows (Build/Windows) is the untested version. 
+
+- Name is added to the HUD in bright pink at the top left corner of the screen.
+- Hidden meta data modified to be shown.
+- Change to the script: When the user reaches ball count 11, the message "Just one more!" is displayed at the bottom of the screen.
+  This message is removed when "You win!" is displayed when reaching count == 12. SetActive false/true is used in the script. 
+- Change to 3D graphics: the player ball was changed to bright pink instead of light blue, which is instructed in the tutorial.
+- Both OSX and Windows version present in the "Build" folder. OSX is the MAIN build and Windows is the UNTESTED version. 
+
+** Note **
+
+There is no bug in the game-play it self but I did confront an issue cleaning up the files.
+
+I am aware that dependent files such as "Library", "Temp" and "Obj" need to be removed for submission.
+However, when I removed these files for submission, my Unity project completely crashed.
+I am fully aware that this is subject to points deduction, but I really did not know what to do after fighting with them for a couple of hours. 
+
+If TA's could leave a feedback for this issue (if it is normal to crash, yet files still need to be deleted for submission only), I would sincerely appreciate it!
+This was my first milestone and I am sure I will get better in cleaning the files from the next submission by referring feedbacks.
+
+Thank you! Any feedback or advice would be appreciated :)
